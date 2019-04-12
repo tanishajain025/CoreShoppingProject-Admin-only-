189bec6693ab9c60bd0c7f4b62a5f023f956a106
tanishajain025 <tanishajain059@gmail.com>
2019-04-12 09:48:41 +0530

    Add project files.

---
+++ CoreShoppingAdminPortal/Models/ProductCategory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreShoppingAdminPortal.Models
+{
+    public class ProductCategory
+    {
+       
+            public int ProductCategoryId { get; set; }
+            public string CategoryName { get; set; }
+            public string CategoryDescription { get; set; }
+
+          public  List<Product> Products { get; set; }
+        }
+    }
+
